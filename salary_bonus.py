fd69f654c06f7d15ee2d4718328fbcafa96e6c98
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-23 16:30:13 +0530

    save to new branch

--- salary_bonus.py
+++ salary_bonus.py
@@ -2,5 +2,7 @@ salary = float(input("Enter employee salary: ₹"))
 bonus = salary * 0.10
 total_salary = salary + bonus
 tax = total_salary *0.05
+final_salary = total_salary - tax
 print("\nOriginal Salary: ₹", salary)
 print("Bonus Added: ₹", total_salary)
+print("after tax deduction :",final_salary)
