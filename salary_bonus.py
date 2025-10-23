eda71629550fae99728fcaa88f5960b08805ee81
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-23 16:26:03 +0530

    Update salary_bonus.py

--- salary_bonus.py
+++ salary_bonus.py
@@ -1,5 +1,6 @@
 salary = float(input("Enter employee salary: ₹"))
 bonus = salary * 0.10
 total_salary = salary + bonus
+tax = total_salary *0.05
 print("\nOriginal Salary: ₹", salary)
 print("Bonus Added: ₹", total_salary)
